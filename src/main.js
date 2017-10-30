ebeab742c5930fcef49c67fd566fa64e374c0fbd
nikolai-laevskii <nikolai.laevskii@yandex.ru>
2017-10-30 10:24:40 +0300

    Переписано на socket.io и Koa

--- src/main.js
+++ src/main.js
@@ -20,72 +20,77 @@
 //
 // Nyan cat lies here...
 //
-const QS = require('querystring');
-const WS = require('ws');
 const LifeGameVirtualDom = require('../lib/LifeGameVirtualDom');
+const Koa = require('koa');
+const app = new Koa();
+const server = require('http').createServer(app.callback());
+const io = require('socket.io')(server, {
+  transport: ['websocket'],
+  path: '/'
+});
+app.use(ctx => {
+  ctx.body = 'sry, websockets only :c'
+});
 
-const socket = new WS.Server({port: 3001});
-
-const gameInstance = new LifeGameVirtualDom();
-gameInstance.sendUpdates = data => {
-  const type = "UPDATE_STATE";
-  try {
-    socket.clients.forEach(client => {
-      if (client.readyState === WS.OPEN) {
-        client.send(JSON.stringify({type, data}));
-      }
-    });
-  } catch (e) {
-    console.log(e.message);
-  }
-};
-
-const executeScenario = (msgdata) => {
+const executeScenario = message => {
   const allowedTypes = ['ADD_POINT'];
-  const type = msgdata.type;
-  const data = msgdata.data;
+  const type = message.type;
+  const data = message.data;
   switch (type) {
     case 'ADD_POINT':
       if (!data)
-        throw new Error(`Message data error. No data.`);
+        throw new Error(`Message data error: no data`);
       gameInstance.applyUpdates(data);
       break;
     default:
-      throw new Error(`Message type error. Expected one of following types: ${allowedTypes}; got: ${type}`);
+      throw new Error(`Message type error: expected one of types ${allowedTypes}; got: ${type}`);
   }
 }
-const generateColor = (color) => {
+const generateColor = () => {
   const getHex = () => Math.floor(Math.random() * 16).toString(16);
-  color = [];
+  let color = [];
   let i = 3;
   while(i) {
     color.push(getHex());
     i--;
   }
-  return `#${color.join('')}`
+  return `#${color.join('')}`;
 }
-const handleConnection = (socket, req) => {
-  const token = QS.parse(req.url.slice(2)).token;
-  const color = (token.match(/^([0-9]|[A-F]){3}$/)) ? `#${token}` : generateColor(); //Если имя позволяет сгенерировать из него цвет, то почему бы и нет
+const handleMessage = message => {
   try {
+    executeScenario(message);
+  }
+  catch (e) {
+    console.log(e.message);
+  }
+}
+const handleConnection = socket => {
+  socket.on('message', handleMessage);
+  try {
+    const token = socket.handshake.query.token;
+    console.log(`Someone connected. Token: ${token}`)
+    const color = (token.match(/^([0-9]|[A-F]){3}$/)) ? `#${token}` : generateColor();
     const data = {
       state: gameInstance.state,
       settings: gameInstance.settings,
-      user: {token, color}
-    };
-    socket.send(JSON.stringify({type: 'INITIALIZE', data}));
+      user: { token, color }
+    }
+    socket.emit('message', {type: "INITIALIZE", data});
   } catch (e) {
     console.log(e.message);
   }
-  const handleMessage = msg => {
-    try {
-      const msgdata = JSON.parse(msg);
-      executeScenario(msgdata);
-    } catch (e) {
-      return console.log(e.message);
-    }
-  }
-  socket.on("message", handleMessage);
 }
 
-socket.on("connection", handleConnection);
+server.listen(3001);
+
+io.on('connection', handleConnection);
+
+const gameInstance = new LifeGameVirtualDom();
+gameInstance.sendUpdates = data => {
+  const type = "UPDATE_STATE";
+  try {
+    io.emit('message', {type, data});
+  } catch (e) {
+    console.log(e.message);
+  }
+};
